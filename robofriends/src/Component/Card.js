d8a35620e854f321fbe360acff66a8b437025dfc
MikaelBrassard <mikaelbrassard@solutioncpa.com>
2019-01-24 20:53:54 -0800

    begin React app

---
+++ robofriends/src/Component/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return(
+    <div>
+        <img alt='img' src='' />
+        <div>
+            <h2>Mikael Brassard</h2>
+            <p>MikaelBrassard@solutioncpa.com</p>
+        </div>
+    </div>
+    );
+}
+
+export default Card;

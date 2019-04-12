91714060c13789e5fd335c799f75d13ae4718988
MikaelBrassard <mikaelbrassard@solutioncpa.com>
2019-04-12 13:05:45 -0700

    RoboFriendsApp done

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import CardList from './Cardlist';
-import SearchBox from './SearchBox';
+import CardList from '../components/Cardlist';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {
@@ -28,23 +29,23 @@ class App extends Component {
     render()
     {
         console.log("Render")
-        const filteredRobot = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state
+        const filteredRobot = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
 
-        if (this.state.robots.length === 0 ) { 
-            return <h1>Loading</h1>
-        }
-        else{
-            return(
+        return !robots.length?
+            <h1>Loading</h1>:
+            (
                 <div className='tc'>
                     <h1>React Robots Project</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
-                    <CardList robots={filteredRobot}/>   
+                    <Scroll>
+                        <CardList robots={filteredRobot}/>   
+                    </Scroll>
                 </div>
                 
             )    
-        }
     }
 
 }
